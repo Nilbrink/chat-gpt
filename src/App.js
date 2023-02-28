9b331d2e2608dd1381c009068e5b360ab415d175
Fredrik Nilbrink <fredrik.nilbrink@ri.se>
2023-02-28 11:31:00 +0100

    Updated interaction

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import {Configuration, OpenAIApi} from 'openai'
 const App = () => {
   const apiKey = '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';
   
-  
-
   return (
     <div>
-      <h1>GPT-3 Demo</h1>
-      <GPT3Component apiKey={apiKey} />
+      <h1>RISE GPT3 Component for React</h1>
+      <GPT3Component readOnly apiKey={apiKey} />
     </div>
   );
 };
--- src/GPT3Component.js
+++ src/GPT3Component.js
@@ -4,11 +4,13 @@ import './App.css';
 
 const GPT3Component = ({ apiKey }) => {
   const [inputText, setInputText] = useState('');
+  const [savedInputText, setSavedInputText] = useState('');
   const [outputText, setOutputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  const myKey = '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';
+  
   const configuration = new Configuration ({
-    apiKey: myKey,
+    apiKey: apiKey,
   });
   const openai = new OpenAIApi(configuration);
   
@@ -16,28 +18,35 @@ const GPT3Component = ({ apiKey }) => {
   
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
     
     const requestOptions = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + String(myKey)
+        'Authorization': 'Bearer ' + String(apiKey)
       },
       body: JSON.stringify({
-        'prompt': 'Brainstorm some ideas combining VR and fitness:',
-        'temperature': 0.1,
-        'max_tokens': 50,
+        'model' : 'text-davinci-003',
+        'prompt': inputText,
+        'temperature': 0.8,
+        'max_tokens': 500,
         'top_p': 1,
-        'frequency_penalty': 0,
+        'frequency_penalty': 1,
         'presence_penalty': 0.5,
         'stop': ["\"\"\""],
       })
     };
-    fetch('https://api.openai.com/v1/engines/code-davinci-001/completions', requestOptions)
+    fetch('https://api.openai.com/v1/completions', requestOptions)
         .then(response => response.json())
         .then(data => {
+          console.log(data)
           setOutputText (data.choices[0].text)
-          console.log(outputText)
+          setSavedInputText(inputText);
+          setInputText('');
+          setIsLoading(false);
+    
+          
       }).catch(err => {
         console.log("Ran out of tokens for today! Try tomorrow!");
       });
@@ -54,7 +63,16 @@ const GPT3Component = ({ apiKey }) => {
         <button type="submit">Generate</button>
       </form>
       <div> 
-      <textarea className='Answer' value={outputText} ></textarea>
+      {
+        isLoading &&
+      <p>Funderar på vad jag ska svara...</p>
+      }
+      {!isLoading &&
+      <div>
+        <p> Input: {savedInputText}</p> 
+        <textarea className='Answer' value={outputText} ></textarea>
+      </div>
+      }
         
       </div>
       
