235c7ef23c554e59fe2ff5d38ee835a9d5c277f4
Fredrik Nilbrink <fredrik.nilbrink@ri.se>
2023-03-09 09:55:06 +0100

    Completed chat function

--- src/GPT3Component.js
+++ src/GPT3Component.js
@@ -1,25 +1,44 @@
 import {Configuration, OpenAIApi } from 'openai';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import './ConvertTime'
+import { ConvertTime } from './ConvertTime';
 
 const GPT3Component = ({ apiKey }) => {
-  const [inputText, setInputText] = useState('');
-  const [savedInputText, setSavedInputText] = useState('');
-  const [outputText, setOutputText] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
   
   const configuration = new Configuration ({
     apiKey: apiKey,
   });
-  const openai = new OpenAIApi(configuration);
+  const openai = new OpenAIApi(configuration);  
+
+  const [inputText, setInputText] = useState('');
+  const [savedInputText, setSavedInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [conversationMessages, setConversationMessages] = useState([]) 
+  const [lastUserMessages, setLastUserMessages] = useState(null)
+  const [lastAIMessages, setlastAIMessages] = useState(null)
+
+  const [messageTitle, setMessageTitle]= useState(null)
   
+  // Clear current conversation
+  const clearConversation = () => {
+    setConversationMessages([])
+    setOutputText('[tog bort konversationen]')
+    setMessageTitle(null)
+
+  }
   
-  
+  // Handle user input submit
   const handleSubmit = async (event) => {
     event.preventDefault();
     setIsLoading(true);
+    var userMessage = {"role": "user", "content": inputText}
+    var newMessage = [...conversationMessages, userMessage]
+    var messageTitle = null
+    setLastUserMessages(userMessage)
     
+
     const requestOptions = {
       method: 'POST',
       headers: {
@@ -27,30 +46,42 @@ const GPT3Component = ({ apiKey }) => {
         'Authorization': 'Bearer ' + String(apiKey)
       },
       body: JSON.stringify({
-        'model' : 'text-davinci-003',
-        'prompt': inputText,
+        'model' : 'gpt-3.5-turbo',
+        "messages": newMessage,
         'temperature': 0.8,
         'max_tokens': 500,
         'top_p': 1,
         'frequency_penalty': 1,
         'presence_penalty': 0.5,
         'stop': ["\"\"\""],
+        
       })
     };
-    fetch('https://api.openai.com/v1/completions', requestOptions)
+    fetch('https://api.openai.com/v1/chat/completions', requestOptions)
         .then(response => response.json())
         .then(data => {
-          console.log(data)
-          setOutputText (data.choices[0].text)
+          setMessageTitle(data.choices[0].message.role + ' ' + ConvertTime(data.created) + '\n')
+          setlastAIMessages(data.choices[0].message)
+          setOutputText(data.choices[0].message.content)
           setSavedInputText(inputText);
           setInputText('');
           setIsLoading(false);
-    
+          console.log(data)
           
       }).catch(err => {
-        console.log("Ran out of tokens for today! Try tomorrow!");
+        console.log("There was an error in the fetch");
       });
   }
+  useEffect( () => {
+    if (lastAIMessages && lastAIMessages)
+      setConversationMessages([...conversationMessages, lastUserMessages, lastAIMessages])
+    }, [lastAIMessages])
+  
+    useEffect( () => {
+      console.log(conversationMessages)
+
+      }, [conversationMessages])
+    
   
 
   return (
@@ -69,8 +100,19 @@ const GPT3Component = ({ apiKey }) => {
       }
       {!isLoading &&
       <div>
+        <p>
+          <p> Konversationen har {conversationMessages.length} meddelanden</p>
+          <button onClick={clearConversation}>
+            Nollställ konversation
+          </button>
+          </p>
         <p> Input: {savedInputText}</p> 
-        <textarea className='Answer' value={outputText} ></textarea>
+        <div className='answer'>
+          <p>
+            {messageTitle}
+            {outputText}
+          </p>
+          </div>
       </div>
       }
         
